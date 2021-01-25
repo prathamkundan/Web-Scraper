ce84121175c1720456a24a1383a06d124b27f5db
unknown <dunkan302>
2021-01-25 14:06:48 +0530

    felxible path

--- web.py
+++ web.py
@@ -1,11 +1,10 @@
 import bs4,requests,os
-from pathlib import Path
-baseUrl='https://timesofindia.indiatimes.com/'
-pages={'':'Index','india':'India','world':'World','business':'Business'}
+baseUrl='https://timesofindia.indiatimes.com'
+pages=['/india','/world','/business']
 def addToNote(url,title):
     #This takes links and stores stuff in the notpad file
     try:
-        write_title=title+'\n\n'
+        write_title=title+'\n'
         ntext=''
         write_date=''
         url=url.strip()
@@ -13,7 +12,7 @@ def addToNote(url,title):
         soup=bs4.BeautifulSoup(content.text,'html.parser')
         date=soup.find('div',{'class':'_3Mkg- byline'})
         if date.text!=None:
-            write_date='Source and Date: '+date.text+'\n\n'
+            write_date='Date: '+date.text[7:]+'\n'
         roughText=soup.find('div',{'class':'ga-headlines'})
         ntext=roughText.text
         f=open('%s.txt' %title,'w',encoding='utf-8')
@@ -21,7 +20,7 @@ def addToNote(url,title):
         f.close()
         return 1
     except:
-#         print('check: ',title)
+        print('check: "',title,'"')
         return 0
 
 def getLinkList(res):
@@ -29,8 +28,6 @@ def getLinkList(res):
     #Populates a dictionary with link and title
     soup=bs4.BeautifulSoup(res.text,'html.parser')
     subText=soup.find('div',{'class':'main-content'})
-    if subText==None:
-        subText=soup.find('div',{'id':'content'})
     mainTags=subText.findAll('a',{'class':None})
     linkDict={}
     for page in mainTags:
@@ -44,27 +41,18 @@ def getLinkList(res):
     return linkDict
 # res=requests.get('https://timesofindia.indiatimes.com/india')
 # dic=getLinkList(res)
-
-def netRunner():
-    count=0
-    actcount=0
-    if ('news' not in os.listdir()):
-        os.mkdir('news')
-    os.chdir(Path(os.getcwd(),'news'))
-    cwd=os.getcwd()
-    for key in pages:
-        if (pages[key] not in os.listdir()):
-            os.mkdir(pages[key])
-    for i in pages:
-        os.chdir(Path(cwd,pages[i]))
-        res=requests.get(baseUrl+i)
-        dic=getLinkList(res)
-        print(len(dic),'procured from',pages[i])
-        actcount+=len(dic)
-        for key in dic:
-            if (key!=None):
-                count+=addToNote(dic[key],key)
-    print ('total:',actcount)
-    print(count,' in readable format.')
-    
-netRunner()
+count=0
+actcount=0
+if ('news' not in os.listdir()):
+    os.mkdir('news')
+os.chdir('.//news')
+for i in pages:
+    res=requests.get(baseUrl+i)
+    dic=getLinkList(res)
+    print(len(dic),'procured from',i)
+    actcount+=len(dic)
+    for key in dic:
+        if (key!=None):
+            count+=addToNote(dic[key],key)
+print ('total:',actcount)
+print(count,' OK')
